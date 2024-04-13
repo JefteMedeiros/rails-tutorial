e917540ace8a12515faaf1b2171c4cc78a3ccdf5
Jefté Medeiros <jefte.medeiros8@gmail.com>
2024-04-12 19:50:01 -0500

    devise main config done

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem "rails", "~> 7.1.3", ">= 7.1.3.2"
 
 gem "sprockets-rails"
 
+gem 'devise', '~> 4.9', '>= 4.9.4'
+
 gem "sqlite3", "~> 1.4"
 
 gem "puma", ">= 5.0"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_04_055345) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_03_033151) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -20,4 +20,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_04_055345) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
